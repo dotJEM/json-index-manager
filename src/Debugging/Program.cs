299100dc86f994111900d9651af538e562fe78e2
Jens Melgaard <admin@it-links.dk>
2023-01-24 20:56:43 +0100

    further

--- src/Debugging/Program.cs
+++ src/Debugging/Program.cs
@@ -1,17 +1,90 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 using System;
+using System.IO;
+using System.IO.Compression;
+using System.Threading.Tasks;
 using DotJEM.Json.Index;
+using DotJEM.Json.Index.Configuration;
 using DotJEM.Json.Index.Manager;
 using DotJEM.Json.Storage;
+using DotJEM.Json.Storage.Configuration;
 using Lucene.Net.Analysis;
+using Lucene.Net.Documents;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
-Console.WriteLine("Hello, World!");
+var storage = new SqlServerStorageContext("Data Source=.\\DEV;Initial Catalog=ssn3db;Integrated Security=True");
+storage.Configure.MapField(JsonField.Id, "id");
+storage.Configure.MapField(JsonField.ContentType, "contentType");
+storage.Configure.MapField(JsonField.Version, "$version");
+storage.Configure.MapField(JsonField.Created, "$created");
+storage.Configure.MapField(JsonField.Updated, "$updated");
+storage.Configure.MapField(JsonField.SchemaVersion, "$schemaVersion");
+
+var index = new LuceneStorageIndex(new LuceneFileIndexStorage(".\\app_data\\index", new WhitespaceAnalyzer()));
+index.Configuration.SetTypeResolver("contentType");
+index.Configuration.SetRawField("$raw");
+index.Configuration.SetScoreField("$score");
+index.Configuration.SetIdentity("id");
+index.Configuration.SetSerializer(new ZipJsonDocumentSerializer());
 
 
+IStorageManager storageManager = new StorageManager(storage);
+IIndexManager manager = new IndexManager(storageManager, index);
+Task runner =Task.Run(async () => await storageManager.Run());
+//Task runner = storage.Run();
 
-IStorageManager storage = new StorageManager(new SqlServerStorageContext("Data Source=.\\DEV;Initial Catalog=ssn3db;Integrated Security=True"));
-IIndexManager manager = new IndexManager(storage, new LuceneStorageIndex(new LuceneFileIndexStorage(".\\app_data\\index", new WhitespaceAnalyzer())) );
+while (true)
+{
+    switch (Console.ReadLine())
+    {
+        case "EXIT":
+            goto EXIT;
+            break;
+
+        default:
+            manager.Flush();
+            break;
+    }
+}
+
+EXIT:
+await runner;
 
 
-await storage.Run();
+public class ZipJsonDocumentSerializer : IJsonDocumentSerializer
+{
+
+    public IFieldable Serialize(string rawfield, JObject value)
+    {
+        using (MemoryStream stream = new MemoryStream())
+        {
+            using (GZipStream zip = new GZipStream(stream, CompressionLevel.Optimal))
+            {
+                JsonTextWriter jsonWriter = new JsonTextWriter(new StreamWriter(zip));
+                value.WriteTo(jsonWriter);
+                jsonWriter.Close();
+            }
+            byte[] buffer = stream.GetBuffer();
+            return new Field(rawfield, buffer, Field.Store.YES);
+        }
+
+
+    }
+
+    public JObject Deserialize(string rawfield, Document document)
+    {
+        byte[] buffer = document.GetBinaryValue(rawfield);
+        using (MemoryStream stream = new MemoryStream(buffer))
+        {
+            using (GZipStream zip = new GZipStream(stream, CompressionMode.Decompress))
+            {
+                JsonTextReader reader = new JsonTextReader(new StreamReader(zip));
+                JObject entity = (JObject)JToken.ReadFrom(reader);
+                reader.Close();
+                return entity;
+            }
+        }
+    }
+}
--- src/DotJEM.Json.Index.Manager/Class1.cs
+++ src/DotJEM.Json.Index.Manager/Class1.cs
@@ -1,188 +1,275 @@
 ﻿using System;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Reactive.Linq;
 using System.Runtime;
 using System.Runtime.Remoting.Contexts;
 using System.Threading.Tasks;
+using DotJEM.Json.Index.Configuration.IdentityStrategies;
 using DotJEM.Json.Index.Manager.Diagnostics;
 using DotJEM.Json.Index.Manager.Observable;
 using DotJEM.Json.Storage;
 using DotJEM.Json.Storage.Adapter;
 using DotJEM.Json.Storage.Adapter.Materialize.ChanceLog;
 using DotJEM.Json.Storage.Adapter.Materialize.ChanceLog.ChangeObjects;
+using DotJEM.Json.Storage.Adapter.Observable;
+using Lucene.Net.Documents;
+using Lucene.Net.Index;
 using Newtonsoft.Json.Linq;
 
-namespace DotJEM.Json.Index.Manager
+namespace DotJEM.Json.Index.Manager;
+
+public interface IQueuedItem
 {
-    public interface IQueuedItem
-    {
 
+}
+
+public interface IStorageChange : IQueuedItem
+{
+    long Generation { get; }
+    ChangeType Type { get; set; }
+    string Area { get; }
+    JObject Entity { get; }
+}
+
+public class StorageChange : IStorageChange
+{
+    public long Generation { get; }
+    public ChangeType Type { get; set; }
+    public string Area { get; }
+    public JObject Entity { get; }
+
+    public StorageChange(IChangeLogRow change)
+    {
+        Generation = change.Generation;
+        Type = change.Type;
+        Area = change.Area;
+        Entity = change.CreateEntity();
+    }
+}
+
+public class CommitStorageChange : IQueuedItem
+{
+}
+
+public interface IStorageObserveable: IObservable<IStorageChange>
+{
+
+}
+
+public interface IStorageManager
+{
+    IInfoStream InfoStream { get; }
+    AbstractObserveable<IQueuedItem> Observeable { get; }
+    Task Run();
+}
+
+public class StorageManager : IStorageManager
+{
+    private readonly IStorageContext context;
+    private readonly StorageObserveable observeable = new StorageObserveable();
+    public AbstractObserveable<IQueuedItem> Observeable => observeable;
+    public IInfoStream InfoStream { get; } = new DefaultInfoStream<StorageManager>();
+
+    public StorageManager(IStorageContext context)
+    {
+        this.context = context;
     }
 
-    public interface IStorageChange : IQueuedItem
+    public async Task Run()
     {
-        long Generation { get; }
-        ChangeType Type { get; set; }
-        string Area { get; }
-        JObject Entity { get; }
+        Console.WriteLine("Starting Storage Manger");
+        await Task.WhenAll(context
+            .AreaInfos
+            .Select(info => {
+                StorageAreaObserver observer = new StorageAreaObserver(context.Area(info.Name));
+                observer.Observeable.Forward(observeable);
+                return observer.Run();
+            })).ConfigureAwait(false);;
+    }
+}
+
+
+public class StorageAreaObserver 
+{
+    private readonly IStorageArea area;
+    private readonly IStorageAreaLog log;
+    private readonly StorageObserveable observeable = new StorageObserveable();
+    public AbstractObserveable<IQueuedItem> Observeable => observeable;
+
+    public IInfoStream InfoStream { get; } = new DefaultInfoStream<StorageAreaObserver>();
+
+    public StorageAreaObserver(IStorageArea area)
+    {
+        this.area = area;
+        this.log = area.Log;
     }
 
-    public class StorageChange : IStorageChange
+    public Task Run()
     {
-        public long Generation { get; }
-        public ChangeType Type { get; set; }
-        public string Area { get; }
-        public JObject Entity { get; }
-
-        public StorageChange(IChangeLogRow change)
+        return Task.Run(async () =>
         {
-            Generation = change.Generation;
-            Type = change.Type;
-            Area = change.Area;
-            Entity = change.CreateEntity();
-        }
-    }
-
-    public class CommitStorageChange : IQueuedItem
-    {
-    }
-
-    public interface IStorageObserveable: IObservable<IStorageChange>
-    {
-
-    }
-
-    public interface IStorageManager
-    {
-        IInfoStream InfoStream { get; }
-        AbstractObserveable<IQueuedItem> Observeable { get; }
-        Task Run();
-    }
-
-    public class StorageManager : IStorageManager
-    {
-        private readonly IStorageContext context;
-        private readonly StorageObserveable observeable = new StorageObserveable();
-        public AbstractObserveable<IQueuedItem> Observeable => observeable;
-        public IInfoStream InfoStream { get; } = new DefaultInfoStream<StorageManager>();
-
-        public StorageManager(IStorageContext context)
-        {
-            this.context = context;
-        }
-
-        public async Task Run()
-        {
-
-            await Task.WhenAll(context
-                .AreaInfos
-                .Select(info => {
-                    StorageAreaObserver observer = new StorageAreaObserver(context.Area(info.Name));
-                    observer.Observeable.Forward(observeable);
-                    return observer.Run();
-                }));
-        }
-    }
-
-
-    public class StorageAreaObserver 
-    {
-        private readonly IStorageArea area;
-        private readonly IStorageAreaLog log;
-        private readonly StorageObserveable observeable = new StorageObserveable();
-        public AbstractObserveable<IQueuedItem> Observeable => observeable;
-
-        public IInfoStream InfoStream { get; } = new DefaultInfoStream<StorageAreaObserver>();
-
-        public StorageAreaObserver(IStorageArea area)
-        {
-            this.area = area;
-            this.log = area.Log;
-        }
-
-        public async Task Run()
-        {
-            Process process = Process.GetCurrentProcess();
+            long gen = 0;
+            bool init = true;
+            Console.WriteLine($"Ingesting {area.Name}");
             while (true)
             {
-                IStorageChangeCollection changes = log.Get(false, 10000);
-
-                foreach (IChangeLogRow change in changes.Partitioned)
-                    observeable.Publish(new StorageChange(change));
-
-                process.Refresh();
-                if (process.PrivateMemorySize64 > (long)int.MaxValue * 4)
+                using IStorageAreaLogReader changes = log.OpenLogReader(gen, !init);
+                foreach (IChangeLogRow change in changes)
                 {
-                    observeable.Publish(new CommitStorageChange());
-                    while (process.PrivateMemorySize64 > (long)int.MaxValue * 2)
-                    {
-                        Console.WriteLine("Working set above treshold, pausing fetch");
-                        await Task.Delay(TimeSpan.FromSeconds(10));
-                        GCSettings.LargeObjectHeapCompactionMode = GCLargeObjectHeapCompactionMode.CompactOnce;
-                        GC.Collect();
-                        process.Refresh();
-                    }
+                    gen = change.Generation;
+                    observeable.Publish(new StorageChange(change));
                 }
 
-                if (changes.Count < 50000)
-                    await Task.Delay(TimeSpan.FromSeconds(10));
+                init = false;
+                await Task.Delay(TimeSpan.FromSeconds(10)).ConfigureAwait(false);
             }
-        }
+        });
+    }
+}
+
+
+public class StorageObserveable : AbstractObserveable<IQueuedItem>
+{
+    public IInfoStream InfoStream { get; } = new DefaultInfoStream<StorageObserveable>();
+}
+
+public interface IIndexManager
+{
+    void Flush();
+}
+
+public class IndexManager : IIndexManager
+{
+    private readonly IStorageManager storageManager;
+    private readonly IStorageIndex index;
+    public IInfoStream InfoStream { get; } = new DefaultInfoStream<IndexManager>();
+
+    public IndexManager(IStorageManager storage, IStorageIndex index)
+    {
+        this.storageManager = storage;
+        this.index = index;
+
+        storage.Observeable
+            .ForEachAsync(CaptureChange);
+
+        context = new LuceneWriteContext(index);
     }
 
-
-    public class StorageObserveable : AbstractObserveable<IQueuedItem>
+    public void Flush()
     {
-        public IInfoStream InfoStream { get; } = new DefaultInfoStream<StorageObserveable>();
+        Console.WriteLine("Flushing buffers!");
+        string buffer = $"[{watch.Elapsed}] {counter} ({counter / watch.Elapsed.TotalSeconds} / sec) => {exceptions}";
+        Console.WriteLine(buffer);
+
+        context.Flush(true, true, true);
+        context.Commit();
+
+        GCSettings.LargeObjectHeapCompactionMode = GCLargeObjectHeapCompactionMode.CompactOnce;
+        GC.Collect();
+        //context.Commit();
     }
 
-    public interface IIndexManager
+    private long counter = 0;
+    private long exceptions = 0;
+    private Stopwatch watch = Stopwatch.StartNew();
+    private readonly LuceneWriteContext context;
+
+    private void CaptureChange(IQueuedItem obj)
     {
-
-    }
-
-    public class IndexManager : IIndexManager
-    {
-        private readonly IStorageManager storageManager;
-        private readonly IStorageIndex index;
-        public IInfoStream InfoStream { get; } = new DefaultInfoStream<IndexManager>();
-
-        public IndexManager(IStorageManager storage, IStorageIndex index)
+        if (obj is IStorageChange chn)
         {
-            this.storageManager = storage;
-
-            storage.Observeable.ForEachAsync(CaptureChange);
-
-            context = index.Writer.WriteContext();
-        }
-
-        private long counter = 0;
-        private Stopwatch watch = Stopwatch.StartNew();
-        private readonly ILuceneWriteContext context;
-
-        private void CaptureChange(IQueuedItem obj)
-        {
-            if (obj is CommitStorageChange)
-            {
-                context.Commit();
-                GCSettings.LargeObjectHeapCompactionMode = GCLargeObjectHeapCompactionMode.CompactOnce;
-                GC.Collect();
+            if(chn.Type == ChangeType.Faulty)
                 return;
-            }
 
-            if (obj is IStorageChange chn)
+            try
             {
-                context.Write(chn.Entity);
-                counter++;
-                if(counter % 10000 != 0)
-                    return;
+                switch (chn.Type)
+                {
+                    case ChangeType.Create:
+                        context.Create(chn.Entity);
+                        break;
+                    case ChangeType.Update:
+                        context.Write(chn.Entity);
+                        break;
+                    case ChangeType.Delete:
+                        context.Delete(chn.Entity);
+                        break;
+                    case ChangeType.Faulty:
+                        break;
+                    default:
+                        throw new ArgumentOutOfRangeException();
+                }
+
             }
-
-
-            string buffer = $"[{watch.Elapsed}] {counter} ({counter / watch.Elapsed.TotalSeconds} / sec)";
-            Console.WriteLine(buffer);
-
+            catch (Exception e)
+            {
+                Directory.CreateDirectory($"app_data/exceptions/{chn.Area}");
+                File.WriteAllText($"app_data/exceptions/{chn.Area}/{chn.Entity["id"]}.json", JObject.FromObject(
+                    new
+                    {
+                        exception = e,
+                        change = chn
+                    }
+                    ).ToString());
+                exceptions++;
+            }
+            counter++;
+            if(counter % 25000 != 0)
+                return;
         }
+
+        string buffer = $"[{watch.Elapsed}] {counter} ({counter / watch.Elapsed.TotalSeconds} / sec) => {exceptions}";
+        Console.WriteLine(buffer);
+
+    }
+}
+
+internal class LuceneWriteContext
+{
+    private readonly IStorageIndex index;
+    private readonly IndexWriter writer;
+    private readonly IDocumentFactory mapper;
+    private readonly IIdentityResolver resolver;
+
+
+    public LuceneWriteContext(IStorageIndex index)
+    {
+        this.index = index;
+        this.writer = index.Storage.Writer;
+        this.mapper = index.Services.DocumentFactory;
+        this.resolver = index.Configuration.IdentityResolver;
+
+        var buffer = writer.GetRAMBufferSizeMB();
+        writer.SetRAMBufferSizeMB(1000);
+    }
+
+    public void Write(JObject entity)
+    {
+        Term term = resolver.CreateTerm(entity);
+        Document doc = mapper.Create(entity);
+        writer.UpdateDocument(term, doc);
+    }
+
+    public void Create(JObject entity)
+    {
+        Document doc = mapper.Create(entity);
+        writer.AddDocument(doc);
+    }
+    public void Delete(JObject entity)
+    {
+        Term term = resolver.CreateTerm(entity);
+        writer.DeleteDocuments(term);
+    }
+
+    public void Commit()
+    {
+        writer.Commit();
+    }
+
+    public void Flush(bool triggerMerge, bool flushDocStores, bool flushDeletes)
+    {
+        writer.Flush(triggerMerge, flushDocStores, flushDeletes);
     }
 }
